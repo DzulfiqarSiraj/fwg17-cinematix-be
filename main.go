9f44b0353be161294b505e9d220512543b006cde
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-05-23 16:27:13 +0700

    feat: config vercel deployment

--- main.go
+++ main.go
@@ -3,28 +3,27 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/putragabrielll/fwg17-cinematix-be/src/routers"
-	"github.com/putragabrielll/fwg17-cinematix-be/src/services"
+	handler "github.com/putragabrielll/fwg17-cinematix-be/api"
 )
 
-func noLink(c *gin.Context) {
-	c.JSON(http.StatusNotFound, &services.ResponseBack{
-		Success: false,
-		Message: "Resource not found!",
-	})
-}
+// func noLink(c *gin.Context) {
+// 	c.JSON(http.StatusNotFound, &services.ResponseBack{
+// 		Success: false,
+// 		Message: "Resource not found!",
+// 	})
+// }
 
 func main() {
-	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:5173", "https://astonishing-cheesecake-81f941.netlify.app", "http://cinematix.pasukanhosting.my.id"},
-		AllowMethods: []string{"GET", "POST", "PATCH", "DELETE"},
-		AllowHeaders: []string{"Authorization"},
-	}))
-	routers.Combine(r)
-	r.NoRoute(noLink)
-	// r.Run("127.0.0.1:9090") // for local
-	r.Run("127.0.0.1:9090") // for production
+	// r := gin.Default()
+	// r.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"http://localhost:5173", "https://astonishing-cheesecake-81f941.netlify.app", "http://cinematix.pasukanhosting.my.id", "https://cinematix-app.netlify.app"},
+	// 	AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE"},
+	// 	AllowHeaders:     []string{"Authorization", "Content-Type", "Accept", "access-control-allow-origin", "access-control-allow-headers"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// }))
+	// routers.Combine(r)
+	// r.NoRoute(noLink)
+	// r.Run(":8080")
+	http.HandleFunc("/", handler.Handler)
 }
